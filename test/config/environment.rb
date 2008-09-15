9a9914bbe8dd06918ee948c44343040fd2f6c990
Jon Stenqvist <jon@jon-stenqvists-macbook-pro.local>
2008-09-15 16:53:50 +0200

    Added gem dependency for httparty. Changed how the plugin is loaded with plugin_paths

--- test/config/environment.rb
+++ test/config/environment.rb
@@ -3,8 +3,7 @@ RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.load_paths += %W( #{Rails.root}/../lib )
-  config.plugin_paths += ['vendor/plugins', "#{Rails.root}/.."]
+  config.plugin_paths += ['vendor/plugins', "#{Rails.root}/../.."]
   config.reload_plugins = true
   config.time_zone = 'UTC'
   config.action_controller.session = {
@@ -12,6 +11,3 @@ Rails::Initializer.run do |config|
     :secret      => '1fa781c6f23c0ffb5eb5c86b72c090196d28e77a3fc16cf35186f423f7b3598da64d1fc4d4270cd9a92b13412610f8d7477b09d3045b386a2d4f39fb96fe571e'
   }
 end
-
-# Execute install hook for the plugin
-require Rails.root + '/../init'
